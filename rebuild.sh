96e048c3d23afe721871047d0ce1c7d439c68747
Alvaro J. Hurtado <alvaroh@aspgems.com>
2016-03-01 18:28:16 +0100

    added permissions on rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -5,3 +5,4 @@ docker rm dockerdrupalstarterkit_web_1
 docker-compose build
 docker-compose up -d
 docker exec -it dockerdrupalstarterkit_web_1 /etc/init.d/ssh start
+docker exec -ti dockerdrupalstarterkit_web_1 chown www-data:www-data /var/www/html/sites/default -R
